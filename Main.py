0deeae01fdd7acbb0845009647f4a5dc0a5276f2
GeoffSpielman <geoff.spielman21@gmail.com>
2016-04-24 18:18:24 +0000

    Prompts user for motor levels, basic error checking and sends formatted string over serial to Arduino. Runs through terminal and shell

--- Main.py
+++ Main.py
@@ -1,36 +1,56 @@
 #!/usr/bin/python
 #Geoff Spielman
 #April 22, 2016
+
 import sys
 import time
-import RPi.GPIO as GPIO
 import serial
-
 ser = serial.Serial('/dev/ttyACM0', 9600)
+#stings and ints for storing left and right motor power
+mLint = 0
+mRint = 0
+mL = ''
+mR = ''
+#prepare for stop
+Stp = '0,0!'
+BAS=bytearray()
+BAS.extend(map(ord, Stp))
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(18, GPIO.OUT)
+def IsAnInt(recString):
+    try:
+        int(recString)
+        return True
+    except ValueError:
+        return False
 
+print('Type "stop" at any time to stop car')
+print('To maintain levels, don\'t enter integer. Valid input: 0<int<255')
 
-##for i in range(1, 11):
-##    print ("Enter something")
-##    input = sys.stdin.readline().strip()
-##    print ('For input %d you entered: %s' %(i, input))
+while mL != 'stop' and mR != 'stop':
+    mL = input('Set left motor power: ')
+    if (mL.lower() != 'stop'):
+        mR = str(input('Set right motor power: '))
+    if (mL.lower() == 'stop' or mR.lower() == 'stop'): #immediately stop
+        ser.write(BAS)
+        sys.exit('Stopped')
+    #if user entered text (button mash), maintain motor levels
+    if IsAnInt(mL):
+        mLint = int(mL)
+        if mLint > 255:
+            mLint = 255
+        if mLint < 0:
+            mLint = 0
+    if IsAnInt(mR):
+        mRint = int(mR)
+        if mRint > 255:
+            mRint = 255
+        if mRint < 0:
+            mRint = 0
 
+    print('\nCurrent Values - Left Motor: %d   Right Motor: %d' %(mLint, mRint))
+    output = str(mLint) + ',' + str(mRint) + '!' #this is how arduino finds values
+    BA = bytearray()
+    BA.extend(map(ord, output))
+    #print (BA)
+    ser.write(BA)
 
-print ('How many times should LED blink?')
-input = sys.stdin.readline().strip()
-BA = bytearray()
-BA.extend(map(ord, input))
-
-print (BA)
-
-ser.write(BA)
-
-##for i in range (1, input + 1):
-##    GPIO.output(18, 1)
-##    time.sleep(0.5)
-##    GPIO.output(18, 0)
-##    time.sleep(0.5)
-
-GPIO.cleanup()
